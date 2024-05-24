2da82f30354b785ffc4734ee64945dfe3a79bf43
Mahmudl-Hasan-870 <mahmudulhasan@Mahmuduls-MacBook-Air.local>
2024-05-24 21:34:39 +0600

    first commit

---
+++ app/src/main/java/com/example/retrofit/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.retrofit;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import android.os.Bundle;
+import java.util.List;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private PostAdapter postAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        JsonPlaceholderAPI jsonPlaceholderAPI = RetrofitClient.getRetrofitInstance().create(JsonPlaceholderAPI.class);
+        Call<List<Post>> call = jsonPlaceholderAPI.getPosts();
+
+        call.enqueue(new Callback<List<Post>>() {
+            @Override
+            public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
+                if (response.isSuccessful()) {
+                    List<Post> posts = response.body();
+                    postAdapter = new PostAdapter(posts);
+                    recyclerView.setAdapter(postAdapter);
+                } else {
+                    // Handle error
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Post>> call, Throwable t) {
+                // Handle failure
+            }
+        });
+    }
+}
